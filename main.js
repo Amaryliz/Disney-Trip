3a3ae57a9605e65bbcdd1436ac47111720d1aa46
Amaryliz <99446700+Amaryliz@users.noreply.github.com>
2022-03-03 17:19:55 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function toggleDarkMode() {
     let bodyElement = document.getElementById
     ("main-background");
-    let darkModeButtonElement = document.getElementById
+    let darkModeBtnElement = document.getElementById
     ("dark-mode-btn");
 
     if (bodyElement.style.background ==="black") {
@@ -10,12 +10,12 @@ function toggleDarkMode() {
             hsla(238, 100%, 71%, 1) 0%,
             hsla(295, 100%, 84%, 1) 100%
           )`;
-        darkModeButtonElement.innerHTML = "Dark Mode"
+        darkModeBtnElement.innerHTML = "Dark Mode";
     }
 
     else {
         bodyElement.style.background = "black";
-        darkModeButtonElement.innerHTML = "Light Mode"
+        darkModeBtnElement.innerHTML = "Light Mode";
     }
     
 }
